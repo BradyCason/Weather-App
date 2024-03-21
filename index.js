7d67a18abf0ba4ffa8571cb5fc3109a782946de8
BradyCason <bradyrcason@gmail.com>
2024-03-21 15:23:16 -0700

    change units

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ const wind = document.querySelector(".wind");
 const backgroundImg = document.querySelector(".background-img");
 const errorMessageDiv = document.querySelector(".error-message-div");
 const errorMessage = document.querySelector(".error-message");
+const unitsButton = document.querySelector(".button-57");
+const unitsButtonText = document.querySelector(".units-button-text")
+const unitsButtonAltText = document.querySelector(".units-button-alt-text")
 
 const weatherAPI = new WeatherAPI();
 const background = new Background();
 
-let units = "_f";
+let units = " °F";
+let data;
 
-form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    let data = await weatherAPI.getCityData(input.value.trim());
-
+function updateUI(){
     if (data){
-        weatherContainer.style.visibility = "visible";
-        errorMessageDiv.style.visibility = "hidden";
 
         weatherContainer.display = "flex";
 
@@ -34,15 +33,20 @@ form.addEventListener("submit", async (event) => {
         country.textContent = data.location.country;
         conditionText.textContent = data.current.condition.text;
         conditionImg.src = data.current.condition.icon;
-        temp.textContent = data.current.temp_f + " °F";
-        feelsLike.innerHTML = "Feels like <b>" + data.current.feelslike_f + " °F</b>";
+
+        if(units == " °F"){
+            temp.textContent = data.current.temp_f + units;
+            feelsLike.innerHTML = "Feels like <b>" + data.current.feelslike_f + units + "</b>";
+        }
+        else{
+            temp.textContent = data.current.temp_c + units;
+            feelsLike.innerHTML = "Feels like <b>" + data.current.feelslike_c + units + "</b>";
+        }
         wind.innerHTML = "Wind: <b>" + data.current.wind_mph + " mph</b>, Direction: <b>" + data.current.wind_dir + "</b>";
     
         background.updateBackground(data);
     }
     else{
-        weatherContainer.style.visibility = "hidden";
-        errorMessageDiv.style.visibility = "visible";
         errorMessage.textContent = "Could not find any data for " + input.value + ". Please search for a city or an area code."
 
         let regData = {current : {cloud : 25, is_day : 1, wind_mph: 10}}
@@ -50,4 +54,37 @@ form.addEventListener("submit", async (event) => {
     }
 
     input.value = "";
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    data = await weatherAPI.getCityData(input.value.trim());
+
+    if(data){
+        weatherContainer.style.visibility = "visible";
+        errorMessageDiv.style.visibility = "hidden";
+    }
+    else{
+        weatherContainer.style.visibility = "hidden";
+        errorMessageDiv.style.visibility = "visible";
+    }
+
+    updateUI();
+})
+
+unitsButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    
+    if(units == " °F"){
+        units = " °C";
+        unitsButtonText.innerHTML = "<b>°C</b>";
+        unitsButtonAltText.innerHTML = "<b>°F</b>";
+    }
+    else{
+        units = " °F";
+        unitsButtonText.innerHTML = "<b>°F</b>";
+        unitsButtonAltText.innerHTML = "<b>°C</b>";
+    }
+
+    updateUI();
 })
